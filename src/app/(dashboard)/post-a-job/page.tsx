436e43fce849deadfabe12ef51115a48667ca478
wildan <wildan78933@gmail.com>
2024-02-29 15:57:42 +0700

    update: integration job

--- src/app/(dashboard)/post-a-job/page.tsx
+++ src/app/(dashboard)/post-a-job/page.tsx
@@ -33,10 +33,22 @@ import InputSkills from "@/components/organisms/InputSkills";
 import CKEditor from "@/components/organisms/CKEditor";
 import InputBenefits from "@/components/organisms/InputBenefits/";
 import { Button } from "@/components/ui/button";
+import useSWR from "swr";
+import { fetcher } from "@/lib/utils";
+import { CategoryJob } from "@prisma/client";
+import { useSession } from "next-auth/react";
+import moment from "moment";
+import { useToast } from "@/components/ui/use-toast";
+import { useRouter } from "next/navigation";
 
 interface PostJobProps {}
 
 const PostJob: FC<PostJobProps> = ({}) => {
+  const { data } = useSWR<CategoryJob[]>("/api/job/categories", fetcher);
+
+  const { data: session } = useSession();
+
+  const { toast } = useToast();
   const [editorLoaded, setEditorLoaded] = useState<boolean>(false);
 
   const form = useForm<z.infer<typeof jobFormSchema>>({
@@ -46,8 +58,43 @@ const PostJob: FC<PostJobProps> = ({}) => {
     },
   });
 
-  const onSubmit = (val: z.infer<typeof jobFormSchema>) => {
-    console.log(val);
+  const router = useRouter();
+
+  const onSubmit = async (val: z.infer<typeof jobFormSchema>) => {
+    try {
+      const body: any = {
+        applicants: 0,
+        benefits: val.benefits,
+        categoryId: val.categoryId,
+        companyId: session?.user.id!!,
+        datePosted: moment().toDate(),
+        description: val.jobDescription,
+        dueDate: moment().add(1, "M").toDate(),
+        jobType: val.jobType,
+        needs: 20,
+        niceToHaves: val.niceToHaves,
+        requiredSkills: val.requiredSkills,
+        responsibility: val.responsibility,
+        roles: val.roles,
+        salaryFrom: val.salaryFrom,
+        salaryTo: val.salaryTo,
+        whoYouAre: val.whoYouAre,
+      };
+
+      await fetch("/api/job", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body),
+      });
+
+      await router.push("/job-listings");
+    } catch (error) {
+      toast({
+        title: "Error",
+        description: "Please try again",
+      });
+      console.log(error);
+    }
   };
 
   useEffect(() => {
@@ -187,18 +234,17 @@ const PostJob: FC<PostJobProps> = ({}) => {
                 <FormItem>
                   <FormLabel>Select Job Categories</FormLabel>
                   <Select onValueChange={field.onChange}>
-                    <SelectTrigger className="w-[450px]">
-                      <SelectValue placeholder="Select Job Categories" />
-                    </SelectTrigger>
+                    <FormControl>
+                      <SelectTrigger className="w-[450px]">
+                        <SelectValue placeholder="Select Job Categories" />
+                      </SelectTrigger>
+                    </FormControl>
                     <SelectContent>
-                      <SelectGroup>
-                        <SelectLabel>Job Categories</SelectLabel>
-                        <SelectItem value="apple">Apple</SelectItem>
-                        <SelectItem value="banana">Banana</SelectItem>
-                        <SelectItem value="blueberry">Blueberry</SelectItem>
-                        <SelectItem value="grapes">Grapes</SelectItem>
-                        <SelectItem value="pineapple">Pineapple</SelectItem>
-                      </SelectGroup>
+                      {data?.map((item: any) => (
+                        <SelectItem key={item.id} value={item.id}>
+                          {item.name}
+                        </SelectItem>
+                      ))}
                     </SelectContent>
                   </Select>
                   <FormMessage />

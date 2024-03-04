f4d13183ead673980200e661ccea8938e6318146
wildan <wildan78933@gmail.com>
2024-03-04 14:54:12 +0700

    add : Integration overview, social links, teams

--- src/app/(dashboard)/job-listings/page.tsx
+++ src/app/(dashboard)/job-listings/page.tsx
@@ -12,33 +12,58 @@ import {
 import { JOB_LISTINGS_COLUMNS, JOB_LISTINGS_DATA } from "@/constans";
 import { Badge } from "@/components/ui/badge";
 import ButtonActionTable from "@/components/organisms/ButtonActionTable";
+import prisma from "../../../../lib/prisma";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { getServerSession } from "next-auth";
+import { Job } from "@prisma/client";
+import { dateFormat } from "@/lib/utils";
+import moment from "moment";
 
 interface JobListingsPageProps {}
 
-const JobListingsPage: FC<JobListingsPageProps> = ({}) => {
+async function getDataJobs() {
+  const session = await getServerSession(authOptions);
+
+  const jobs = prisma.job.findMany({
+    where: {
+      companyId: session?.user.id,
+    },
+  });
+
+  return jobs;
+}
+
+const JobListingsPage: FC<JobListingsPageProps> = async ({}) => {
+  const jobs = await getDataJobs();
+
+  console.log(jobs);
+
   return (
     <div>
       <div className="font-semibold text-3xl">Job Listings</div>
       <div className="mt-10">
         <Table>
-          <TableCaption>A list of your recent invoices.</TableCaption>
           <TableHeader>
             <TableRow>
               {JOB_LISTINGS_COLUMNS.map((item: string, i: number) => (
-                <TableHead key={item + 1}>{item}</TableHead>
+                <TableHead key={item + i}>{item}</TableHead>
               ))}
               <TableHead>Action</TableHead>
             </TableRow>
           </TableHeader>
           <TableBody>
-            {JOB_LISTINGS_DATA.map((item: any, i: number) => (
+            {jobs.map((item: Job, i: number) => (
               <TableRow key={item.roles + i}>
                 <TableCell>{item.roles}</TableCell>
                 <TableCell>
-                  <Badge>{item.status}</Badge>
+                  {moment(item.datePosted).isBefore(item.dueDate) ? (
+                    <Badge>Live</Badge>
+                  ) : (
+                    <Badge variant="destructive">Expired</Badge>
+                  )}
                 </TableCell>
-                <TableCell>{item.datePosted}</TableCell>
-                <TableCell>{item.dueDate}</TableCell>
+                <TableCell>{dateFormat(item.datePosted)}</TableCell>
+                <TableCell>{dateFormat(item.dueDate)}</TableCell>
                 <TableCell>
                   <Badge variant="outline">{item.jobType}</Badge>
                 </TableCell>
